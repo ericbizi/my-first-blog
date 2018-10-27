988c75b1764226c0ded7919952efce6e11e1792f
ericbizi <40618230+ericbizi@users.noreply.github.com>
2018-10-27 12:20:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = angular.module('angjs', []);
 
-app.controller('MainController', function($scope, $http) {
+app.controller('MainController', function($scope, $http, $interval, $log,
+  $anchorScroll, $location) {
 
   var onUserComplete = function(response) {
 
@@ -10,20 +11,41 @@ app.controller('MainController', function($scope, $http) {
   };
   var onRepos = function(response) {
     $scope.repos = response.data;
+    $location.hash("userDetails");
+    $anchorScroll();
   };
 
   var onError = function(reason) {
     $scope.error = "Oops. Could not fetch the user.";
   };
+  // A function that will decrement the count
+  var decrementCountdown = function() {
+    $scope.countdown -= 1;
+    if ($scope.countdown < 1) {
+      $scope.search($scope.username);
+    }
+  };
+  var countdownInterval = null;
+  var startCountdown = function() {
+    countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown)
+  };
+
   $scope.search = function(username) {
+    $log.info("Searching for " + username);
     $http.get("https://api.github.com/users/" + username).then(onUserComplete, onError);
+    if (countdownInterval) {
+      $interval.cancel(countdownInterval);
+      $scope.countdown = null;
+    }
 
   };
-  $scope.username = '';
+  $scope.username = 'angular';
   $scope.repoSortOrder = "-stargazers_count";
-
+  $scope.countdown = 10;
+  startCountdown();
 
 });
+
 
 app.filter('capitalize', function() {
   return function(input) {
